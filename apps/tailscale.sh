b76aa8c7efa69fc8d2edf4bee03d2c3a29d450c3
Brandon Marrow <brandon.marrow@gmail.com>
2025-01-02 16:53:06 -0500

    Made tailscale installation adapt to ubuntu release.

--- apps/tailscale.sh
+++ apps/tailscale.sh
@@ -1,5 +1,6 @@
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/xenial.gpg | sudo apt-key add -
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/xenial.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+ubuntu_release="$(lsb_release -c | cut -f2)"
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/"${ubuntu_release}".gpg | sudo apt-key add -
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/"${ubuntu_release}".list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
 sudo apt-get update
 sudo apt-get install tailscale
