431ae136e273de10fd15a14fcf41970805d18ac3
Brandon Marrow <brandon.marrow@gmail.com>
2025-01-05 16:33:37 -0500

    Fixed broken applications directory creation.

--- apps/app_image.sh
+++ apps/app_image.sh
@@ -2,12 +2,12 @@ sudo add-apt-repository ppa:appimagelauncher-team/stable
 sudo apt install appimagelauncher
 
 mkdir /tmp/app-images
-sudo mkdir "${USER}/Applications"
+sudo mkdir "${HOME}/Applications"
 cd /tmp/app-images
 
 curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r '.assets[] | select(.name | endswith(".appimage")) | .browser_download_url' | xargs curl -O
 sudo mv ./nvim.appimage /usr/local/bin/nvim
 
 curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | jq -r '.assets[] | select(.name | endswith(".AppImage") and (contains("arm") | not)) | .browser_download_url' | xargs curl -O
-sudo mv ./* "${USER}"/Applications
+sudo mv ./* "${HOME}"/Applications
 cd -
