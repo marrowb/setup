4eb8669adb74c12518ce74eb3aa51901ad233c18
Brandon Marrow <brandon.marrow@gmail.com>
2025-01-06 12:04:40 -0500

    Moved neovim installation into a separate script.

--- apps/app_image.sh
+++ apps/app_image.sh
@@ -5,9 +5,6 @@ mkdir /tmp/app-images
 sudo mkdir "${HOME}/Applications"
 cd /tmp/app-images
 
-curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r '.assets[] | select(.name | endswith(".appimage")) | .browser_download_url' | xargs curl -O
-sudo mv ./nvim.appimage /usr/local/bin/nvim
-
 curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | jq -r '.assets[] | select(.name | endswith(".AppImage") and (contains("arm") | not)) | .browser_download_url' | xargs curl -O
 sudo mv ./* "${HOME}"/Applications
 cd -
